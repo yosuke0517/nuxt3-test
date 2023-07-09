1f8fd7b1e6dd4b04788663320486bc7afc2863e4
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-07-09 19:12:42 +0900

    feat: prettierとか最低限導入

---
+++ .eslintrc.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@vue/prettier',
+  ],
+
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+}
