cf2d0c33b6efe86ec0955f4b7e4d4f6c34d5c23d
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-07-23 14:58:50 +0900

    feat: test pinia

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,30 @@
 import { defineStore } from 'pinia'
+import { useNuxtApp } from 'nuxt/app'
+
+type TState = {
+  counter: number
+}
+const initialState: TState = {
+  counter: 0,
+}
 
 export const useMainStore = defineStore('main', {
-    state: () => ({
-        counter: 0,
-    }),
-    actions: {
-        increment() {
-            // `this` is the store instance
-            this.counter++
-        },
+  state: () => initialState,
+  actions: {
+    increment() {
+      // `this` is the store instance
+      this.counter++
     },
+  },
+  getters: {
+    getCounter() {
+      const app = useNuxtApp()
+      console.log('getter useNuxtApp', app)
+      return this.counter
+    },
+    getCounter2: (state) => {
+      console.log('getter state', state.counter)
+      return state.counter
+    },
+  },
 })
