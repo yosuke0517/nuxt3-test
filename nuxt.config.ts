fb46897e0b525b3696e9805403660d248d4608e6
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-07-23 14:59:34 +0900

    feat: meta, pinia, dayjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt 3 basic',
+      meta: [{ name: 'description', content: 'Nuxt 3 for beginners' }],
+    },
+  },
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: [
-    'dayjs-nuxt',
-    '@pinia/nuxt',
-  ]
+  modules: ['dayjs-nuxt', '@pinia/nuxt'],
 })
