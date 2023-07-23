af63787977a61d99104d79381990d31406a416fd
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-07-23 15:02:13 +0900

    feat: inject

---
+++ src/plugins/test.ts
@@ -0,0 +1,38 @@
+import { defineNuxtPlugin, useNuxtApp, useRoute } from '#app'
+import { toRaw } from '#imports'
+
+interface PluginsInjections {
+  $myPlugin: MyPluginInterface
+}
+
+declare module '#app' {
+  interface NuxtApp extends PluginsInjections {}
+}
+
+export interface MyPluginInterface {
+  hoge(): string
+}
+
+class MyPlugin implements MyPluginInterface {
+  hoge() {
+    // この中では、Nuxtインスタンスやルートオブジェクトも取得可能
+    console.log(useNuxtApp())
+    console.log(useRoute().query)
+    console.log(toRaw(useRoute().meta))
+    console.log('ほげ')
+
+    return 'hoge'
+  }
+}
+
+export default defineNuxtPlugin(() => {
+  const my = () => {
+    return new MyPlugin()
+  }
+
+  return {
+    provide: {
+      myPlugin: my(),
+    },
+  }
+})
---
+++ src/plugins/test2.ts
@@ -0,0 +1,45 @@
+import { NuxtApp, useNuxtApp, useRoute, useRouter } from '#app'
+
+interface PluginsInjections {
+  $myPlugin2: MyPluginInterface
+}
+
+declare module '#app' {
+  interface NuxtApp extends PluginsInjections {}
+}
+
+export interface MyPluginInterface {
+  fuga(): string
+  context: NuxtApp
+}
+
+class MyPlugin2 implements MyPluginInterface {
+  context: NuxtApp
+  constructor(context: NuxtApp) {
+    this.context = context
+  }
+  fuga() {
+    // この中では、Nuxtインスタンスやルートオブジェクトも取得可能
+    console.log(useNuxtApp())
+    console.log('userRouter', useRouter())
+    console.log('userRoute', useRoute())
+    console.log('ふが')
+
+    return 'fuga'
+  }
+}
+
+export default (nuxtApp: NuxtApp) => {
+  console.log('引数のNuxtApp', nuxtApp)
+  const hoge = useNuxtApp()
+  console.log('useNuxtApp()', hoge)
+  const my = () => {
+    return new MyPlugin2(nuxtApp)
+  }
+
+  return {
+    provide: {
+      myPlugin2: my(),
+    },
+  }
+}
