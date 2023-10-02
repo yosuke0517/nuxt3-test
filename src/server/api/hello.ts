dcaa126f655b768c6b64fb01be7683cb8bba6e10
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2023-10-02 20:43:01 +0900

    fix: setHeaderの取得場所変更

--- src/server/api/hello.ts
+++ src/server/api/hello.ts
@@ -1,7 +1,9 @@
 import { defineEventHandler } from 'h3'
 
 export default defineEventHandler((event) => {
-  event.res.setHeader('Set-Cookie', ['id_token=token_value; HttpOnly; Path=/;'])
+  event.node.res.setHeader('Set-Cookie', [
+    'id_token=token_value; HttpOnly; Path=/;',
+  ])
 
   return {
     hello: 'world',
